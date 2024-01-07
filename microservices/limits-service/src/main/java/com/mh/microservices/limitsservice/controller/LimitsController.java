6d911b0474fd8f080a3310958b1b04ed7758f33c
Mukbul Hossain <mukbul.hossain@commlinkinfotech.com>
2024-01-08 01:07:24 +0600

    Microservice project added. initial commit done

---
+++ microservices/limits-service/src/main/java/com/mh/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.mh.microservices.limitsservice.controller;
+
+
+import com.mh.microservices.limitsservice.bean.Limits;
+import com.mh.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+
+    @Autowired
+    private Configuration configuration;
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+        //return new Limits(1, 100);
+    }
+
+}
